7f8148cc253434bf059a0844e40cad3f81d93d32
AFCMS <afcm.contact@gmail.com>
2025-03-27 12:10:38 +0100

    Basic game server communication

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,10 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 CMD [ "python", "/app/main.py" ]
 
-COPY ./generated /app/generated
+EXPOSE 9990
+
+COPY ./server_pb2_grpc.py /app/server_pb2_grpc.py
+COPY ./server_pb2.py /app/server_pb2.py
+COPY ./server_pb2.pyi /app/server_pb2.pyi
 
 COPY ./main.py /app/main.py
--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+from concurrent import futures
+import grpc
+import server_pb2
+import server_pb2_grpc
 
+
+class GameServer(server_pb2_grpc.GameServerServicer):
+
+    def GameList(self, request, context):
+        return server_pb2.GameListReply(status=True, id_games=[1, 2, 3])
+
+    def GameSubscribe(self, request, context):
+        return server_pb2.GameSubscribeReply(
+            id_player=1, role=server_pb2.Role.Wolf, status=True
+        )
+
+
+if __name__ == "__main__":
+    port = "9990"
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server_pb2_grpc.add_GameServerServicer_to_server(GameServer(), server)
+    server.add_insecure_port("[::]:" + port)
+    server.start()
+    print("Server started, listening on " + port)
+    server.wait_for_termination()
